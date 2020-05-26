d2ce4b7a732a36c37ab81f4d3596326a17521d13
raspi-sdk <klaus.zahn@hslu.ch>
2020-05-26 09:13:47 +0200

    comments ZaK

--- image_processing.cpp
+++ image_processing.cpp
@@ -29,22 +29,94 @@ int CImageProcessor::DoProcess(cv::Mat* image) {
 
         int vmin, vmax, smin;
 
+        
+        int width = 350;
+        int height = 150;
+        
+        char msg[100];
+        
         vmin = m_valueMin;
         vmax = m_valueMax;
         smin = m_saturationMin;
+    
+        m_trackWindow = cv::Rect((image->cols - width) / 2, (image->rows - height) / 2, width, height);
+        
+        
 
-        if (!image) return (EINVALID_PARAMETER);
-
+    if (image->channels() > 1) {
 
         if (!m_startButton) {
-            cv::subtract(cv::Scalar::all(255), *image,*m_proc_image[0]);
+            
+            *m_proc_image[0] = image->clone();
+
+            
+            cv::rectangle( *m_proc_image[0], m_trackWindow, cv::Scalar(0,0,255), 3, cv::LINE_AA);
+            
+            cv::Mat hsv;
+            cv::cvtColor(*image, hsv, cv::COLOR_BGR2HSV);
+
+            cv::Mat roi = hsv(m_trackWindow);
+            cv::Scalar roi_mean = cv::mean(roi);
+            
+            snprintf(msg, 100, "mean values are: h = %3.1f, s = %3.1f, v = %3.1f", roi_mean[0], roi_mean[1], roi_mean[2]);
+            cv::putText(*m_proc_image[0], msg, cv::Point(10,20), cv:: FONT_HERSHEY_PLAIN, 1, cv::Scalar(0,0,255));
+
+            cv::Mat hue;
+            int ch[] = {0,0};
+            hue.create(roi.size(), roi.depth());
+            cv::mixChannels(&roi, 1, &hue, 1, ch, 1);
+            
+            int hsize = 16;
+            float hranges[] = {0, 180};
+            const float* phranges = hranges;
+            
+            cv::calcHist(&hue, 1, 0, hue, m_hist, 1, &hsize, &phranges);
+            cv::normalize(m_hist, m_hist, 0, 255, cv::NORM_MINMAX);
+            
+            //std::cout << m_hist;
+            
+            
         } else {
-            cv::add(cv::Scalar::all(100), *image,*m_proc_image[0]);
+           
+            cv::Mat hsv;
+            cv::cvtColor(*image, hsv, cv::COLOR_BGR2HSV);
+            
+            cv::Mat hue;
+            int ch[] = {0,0};
+            hue.create(hsv.size(), hsv.depth());
+            cv::mixChannels(&hsv, 1, &hue, 1, ch, 1);
+            
+            cv::Mat backproj;
+            float hranges[] = {0,180};
+            const float* phranges = hranges;
+            
+            cv::calcBackProject(&hue, 1, 0, m_hist, backproj, &phranges);
+            //ZaK: good pactice to use debug output :-)
+            *m_proc_image[1] = backproj;
+            
+            cv::Mat mask;
+            cv::inRange(hsv, cv::Scalar(0, smin, MIN(vmin, vmax)), cv::Scalar(180, 256, MAX(vmin, vmax)), mask);
+            
+            backproj &= mask;
+            
+            *m_proc_image[2] = mask;
+            
+            *m_proc_image[0] = image->clone();
+            
+            cv::RotatedRect trackBox = cv::CamShift(backproj, m_trackWindow, cv::TermCriteria(cv::TermCriteria::EPS |
+                    cv::TermCriteria::COUNT, 10, 1));
+            
+            cv::ellipse(*m_proc_image[0], trackBox, cv::Scalar(0,0,255), 3, cv::LINE_AA);
+            
         }
 
-        std::cout << "counter: " << m_counter << ", vmin: " << vmin << ", vmax: " << vmax << ", smin: " << smin << std::endl; 
+        std::cout << "counter: " << m_counter << ", vmin: " << vmin << ", vmax: " << vmax << ", smin: " << smin << std::endl;
 
         m_counter++;
+
+    }
+        
+
 
         return (SUCCESS);
 }
